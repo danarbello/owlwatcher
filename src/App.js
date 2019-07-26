a30d2c20cb1343089f52f3947e83b1a159900736
James Mejia <james.mejia@bixal.com>
2019-07-26 19:57:01 -0400

    Install lodash and get livematch data.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
+import * as _ from 'lodash';
 
 function App() {
   const [data, setData] = useState({ data: [] });
@@ -8,7 +9,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://api.overwatchleague.com/stats/players'
+        'https://api.overwatchleague.com/live-match?expand=team.content&locale=en-us'
       );
 
       setData(result.data);
@@ -17,33 +18,24 @@ function App() {
     fetchData();
   }, []);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Player Name</th>
-          <th>Role</th>
-          <th>Team</th>
-          <th>
-            K/D ratio <small>(Avg. per 10min)</small>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.data.map(player => (
-          <tr key={player.playerId}>
-            <td>{player.name}</td>
-            <td>{player.role}</td>
-            <td>{player.team}</td>
-            <td>
-              {player.eliminations_avg_per_10m.toFixed(2)}/
-              {player.deaths_avg_per_10m.toFixed(2)}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+  const liveMatches = data.data.liveMatch;
+
+  // If data exists show it, otherwise don't bother.
+  if (!_.isEmpty(liveMatches)) {
+    return _.map(liveMatches.competitors, competitor => {
+      return (
+        // We need to check the `STATE` and ping server every X seconds if "IN PROGRESS"
+        <div>
+          <img src={competitor.icon} alt="" height="60" width="60" />
+          <h1>
+            {competitor.name}, <small>{competitor.addressCountry}</small>
+          </h1>
+        </div>
+      );
+    });
+  } else {
+    return null;
+  }
 }
 
 export default App;
