fc8dd9bd4b042dca3f104171027204ab0e29587c
James Mejia <james.mejia@bixal.com>
2020-01-17 19:44:34 -0600

    Stub livematch player, upcoming match, and fallback.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
 import * as _ from 'lodash';
+import Player from './components/Player';
 
 function App() {
   const [data, setData] = useState({ data: [] });
@@ -9,7 +10,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://api.overwatchleague.com/live-match?expand=team.content&locale=en-us'
+        'https://api.overwatchleague.com/live-match'
       );
 
       setData(result.data);
@@ -19,23 +20,18 @@ function App() {
   }, []);
 
   const liveMatches = data.data.liveMatch;
+  const upcomingMatch = _.isEmpty(data.data.nextMatch) ? 'No upcoming matches either' : 'Some upcoming match component';
 
   // If data exists show it, otherwise don't bother.
-  if (!_.isEmpty(liveMatches)) {
-    return _.map(liveMatches.competitors, competitor => {
-      return (
-        // We need to check the `STATE` and ping server every X seconds if "IN PROGRESS"
-        <div>
-          <img src={competitor.icon} alt="" height="60" width="60" />
-          <h1>
-            {competitor.name}, <small>{competitor.addressCountry}</small>
-          </h1>
-        </div>
-      );
-    });
+  if ((!_.isEmpty(data.data))) {
+    if (!_.isEmpty(liveMatches)) {
+      return <Player />;
+    } else {
+    return <React.Fragment>{ upcomingMatch }</React.Fragment>
+    }
   } else {
     return null;
   }
-}
+};
 
 export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as _ from 'lodash';
+
+const Player = () => <div>We'll have to pass data here</div>;
+
+export default Player;
